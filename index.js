7c555429e65d399a0f361eb287c31c8ae7d38d1b
emereson <escanorrosa123@gmail.com>
2024-06-19 18:31:38 -0500

    hola

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ import express from 'express';
 import helmet from 'helmet';
 import { uploadImage, deleteImage } from './controllers/imageController.js';
 import { upload } from './utils/image.multer.js';
-import xss from 'xss-clean';
-import hpp from 'hpp';
 
 const app = express();
-app.use(helmet());
 app.use(express.json());
 app.use(cors());
-app.use(xss());
-app.use(hpp());
 
+app.use(
+  helmet({
+    crossOriginResourcePolicy: false,
+  })
+);
 
-const port = 3115 || 3115;
+const port = 3111 || 3112;
 
 
 app.post('/image', upload.single('image'), uploadImage);

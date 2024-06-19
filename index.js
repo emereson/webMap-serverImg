034cfdf3b138549f58f7efe5393302d0d5d4cc41
emereson <escanorrosa123@gmail.com>
2024-06-19 17:47:50 -0500

    hola

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ import express from 'express';
 import helmet from 'helmet';
 import { uploadImage, deleteImage } from './controllers/imageController.js';
 import { upload } from './utils/image.multer.js';
+import xss from 'xss';
+import hpp from 'hpp';
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(xss());
+app.use(hpp());
 
 app.use(
   helmet({
